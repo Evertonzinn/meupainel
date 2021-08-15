9539e092645e1288fd93ed2fb3367953f09bab08
Everton Comel <60531398+Evertonzinn@users.noreply.github.com>
2021-08-14 22:27:45 -0300

    Add files via upload

---
+++ painel-login-Cad-aute/JS/js.js
@@ -0,0 +1,13 @@
+let userLogado = JSON.parse (localStorage.getItem('userLogado')) 
+let logado = document.querySelector('#logado')
+logado.innerHTML =  `Olá ${userLogado.nome}`
+
+if(localStorage.getItem('token') == null){
+    alert('Você precisa estar logado para acessar esta pagina!')
+    window.location.href = "login.html"
+}
+
+function sair () {
+    localStorage.removeItem('token')
+    window.location.href = "login.html"
+}
